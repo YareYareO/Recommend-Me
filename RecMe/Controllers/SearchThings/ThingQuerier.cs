bc6b9a25f1ae5bf65bef902689f7514bb82d3482
vontr <vontrajolta@protonmail.com>
2023-07-25 13:47:46 +0200

    On the page "Things" you can search for thigns based on one of their tags now. searching "manga" returns berserk for example

---
+++ RecMe/Controllers/SearchThings/ThingQuerier.cs
@@ -0,0 +1,28 @@
+﻿using RecMe.Models;
+
+namespace RecMe.Controllers.SearchThings
+{
+    public class ThingQuerier
+    {
+        private readonly RecMe.Data.RecMeContext _context;
+        public ThingQuerier(RecMe.Data.RecMeContext context) 
+        {
+            _context = context;
+        }
+        public IQueryable<Thing> GetAllThings()
+        {
+            var things = from thing in _context.Thing
+                         select thing;
+            return things;
+        }
+        public IQueryable<Thing> GetThingsByTag(string searchString)
+        {   //finds things that have a tag that contains ONE searchstring
+            var things = from thing in this.GetAllThings()
+                         from tag in _context.Tag
+                         from tht in _context.ThingHasTag
+                         where tht.TagId == tag.Id && tag.Name.Contains(searchString) && thing.Id == tht.ThingId
+                         select thing;
+            return things;
+        }
+    }
+}
--- RecMe/Pages/Things/Index.cshtml.cs
+++ RecMe/Pages/Things/Index.cshtml.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
+using RecMe.Controllers.SearchThings;
 using RecMe.Data;
 using RecMe.Models;
 
@@ -12,21 +14,29 @@ namespace RecMe.Pages.Things
 {
     public class IndexModel : PageModel
     {
-        private readonly RecMe.Data.RecMeContext _context;
+        //private readonly RecMe.Data.RecMeContext _context;
+        private readonly ThingQuerier querier;
 
         public IndexModel(RecMe.Data.RecMeContext context)
         {
-            _context = context;
+            //_context = context;
+            querier = new ThingQuerier(context);
         }
 
         public IList<Thing> Thing { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            if (_context.Thing != null)
-            {
-                Thing = await _context.Thing.ToListAsync();
+            var things = querier.GetAllThings();
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {   
+                things = querier.GetThingsByTag(SearchString);
             }
+            Thing = await things.ToListAsync();
         }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
     }
 }
