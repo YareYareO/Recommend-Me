f3947d0a93208c26dc23dcfc52ed141cb82efe7b
vontr <vontrajolta@protonmail.com>
2023-07-29 17:16:24 +0200

    new migration. tags have now parent tags

---
+++ RecMe/Migrations/20230729144537_ParentTagAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecMe.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParentTagAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "Tag",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Tag");
+        }
+    }
+}
